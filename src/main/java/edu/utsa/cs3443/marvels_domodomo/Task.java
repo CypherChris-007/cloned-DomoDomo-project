0eba2f5f6e10303672d74a27caead926f806d50d
CypherChris-007 <chrisb.college1010@gmail.com>
2026-04-14 21:15:21 -0500

    Completed Task Class | Completed Task Manager: features includes add,remove,edit (With index logic & update task)

--- src/main/java/edu/utsa/cs3443/marvels_domodomo/Task.java
+++ src/main/java/edu/utsa/cs3443/marvels_domodomo/Task.java
@@ -1,5 +1,27 @@
+/**
+ * @author: Chris Jimenez
+ *
+ * @purpose: The Task represents a single to-do item.
+ * It stores the completion status (taskIsCompleted) and has a constructor + toString().
+ *
+ */
 package edu.utsa.cs3443.marvels_domodomo;
 
 public class Task {
+//Variables
+private boolean taskIsCompleted;
+//Constructor
+    public Task(boolean pTaskIsCompleted) {
+        this.taskIsCompleted = pTaskIsCompleted;
+    }
+    //toString | taskIsCompleted -> True or False
+    @Override
+    public String toString() {
+        if (taskIsCompleted) {
+            return "True";
+        } else {
+            return "False";
+        }
 
+    }
 }
