72936d5006069850713b8fb7a89ea38aabd4fa1a
Giovanni De Leon <giothermal1234@gmail.com>
2026-04-21 14:51:15 -0500

    deleted minor comment

--- src/main/java/edu/utsa/cs3443/marvels_domodomo/DomoDomoApplication.java
+++ src/main/java/edu/utsa/cs3443/marvels_domodomo/DomoDomoApplication.java
@@ -7,16 +7,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-//public class DomoDomoApplication extends Application {
-//    @Override
-//    public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(DomoDomoApplication.class.getResource("Edit-screen.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//}
 public class DomoDomoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
